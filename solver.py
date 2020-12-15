59d18726b3b2616b480953a37f6c66673fb841c7
sahar <sahoor.moon22@gmail.om>
2020-12-15 09:23:13 +0200

    final

--- solver.py
+++ solver.py
@@ -10,9 +10,10 @@ def solve(bo):
     for i in range(1,10):
         if valid(bo, i, (row, col)):
             bo[row][col] = i
-
             if solve(bo):
+                
                 return True
+
 
             bo[row][col] = 0
 
